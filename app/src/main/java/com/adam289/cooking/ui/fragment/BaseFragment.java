78f2c2413ef86e8fa4eb9dde6b35f99ac2efed59
Adam289 <646102528@qq.com>
2017-03-21 06:47:03 +0800

    新增主页fragment和magicIndicator

--- app/src/main/java/com/adam289/cooking/ui/fragment/BaseFragment.java
+++ app/src/main/java/com/adam289/cooking/ui/fragment/BaseFragment.java
@@ -12,26 +12,36 @@ import com.adam289.cooking.presenter.Presenter;
 import butterknife.ButterKnife;
 
 /**
- * Created by temp on 2017/3/20.
+ * @ author adam289
+ * @ desc fragment基类
+ * @ created at 2017/3/21 6:00
  */
-
 public abstract class BaseFragment extends Fragment {
     protected Presenter presenter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(getLayoutId(),container,false);
-        ButterKnife.bind(this,view);
-        initView(inflater,container,savedInstanceState);
+        View view = inflater.inflate(getLayoutId(), container, false);
+        ButterKnife.bind(this, view);
+        initView(inflater, container, savedInstanceState);
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if(presenter==null&&getPresenter()!=null){
+        if (presenter == null && getPresenter() != null) {
             presenter = getPresenter();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (presenter != null) {
+            presenter.destory();
         }
     }
 
--- app/src/main/java/com/adam289/cooking/ui/fragment/MainFragment.java
+++ app/src/main/java/com/adam289/cooking/ui/fragment/MainFragment.java
@@ -1,11 +1,95 @@
 package com.adam289.cooking.ui.fragment;
 
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.adam289.cooking.R;
+import com.adam289.cooking.presenter.Presenter;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
- * Created by temp on 2017/3/20.
+ * Desc 主页面Fragment
+ * Created by Adam289 on 2017/3/21.
  */
+public class MainFragment extends BaseFragment {
+    @BindView(R.id.magic_indicator)
+    MagicIndicator magicIndicator;
+    @BindView(R.id.view_pager)
+    ViewPager mViewPager;
+    Context mContext ;
+    private List<String> mTitleDataList;
 
-public class MainFragment extends Fragment {
+    @Override
+    protected Presenter getPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mContext = getActivity();
+        initIndicator();
+    }
+
+    private void initIndicator() {
+        mTitleDataList = new ArrayList<>();
+        mTitleDataList.add("小吃");
+        mTitleDataList.add("湘菜");
+        mTitleDataList.add("粤菜");
+        CommonNavigator commonNavigator = new CommonNavigator(mContext);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+
+            @Override
+            public int getCount() {
+                return mTitleDataList == null ? 0 : mTitleDataList.size();
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                ColorTransitionPagerTitleView colorTransitionPagerTitleView = new ColorTransitionPagerTitleView(context);
+                colorTransitionPagerTitleView.setNormalColor(Color.GRAY);
+                colorTransitionPagerTitleView.setSelectedColor(Color.BLACK);
+                colorTransitionPagerTitleView.setText(mTitleDataList.get(index));
+                colorTransitionPagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mViewPager.setCurrentItem(index);
+                    }
+                });
+                return colorTransitionPagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator indicator = new LinePagerIndicator(context);
+                indicator.setMode(LinePagerIndicator.MODE_WRAP_CONTENT);
+                return indicator;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_main;
+    }
 }
